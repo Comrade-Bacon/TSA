aca509096ce400f439ccc73bf790545329cb344e
Comrade-Bacon <120524043+Comrade-Bacon@users.noreply.github.com>
2023-10-10 12:06:30 -0600

    Add most necessary extensions and create mouse cursor

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
  
+
+// function for the main menue
+function mainMenue() {
+    let cursor = sprites.create(assets.image`Cursor`,SpriteKind.Player) // create mouse cursor
+    controller.moveSprite(cursor, 150, 150) // move mouse withe arrows
+}
+
+mainMenue() // start the  game
