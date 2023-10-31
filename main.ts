98d22f13561c4bc1b310cc712c70322fa4f5a7da
Comrade-Bacon <120524043+Comrade-Bacon@users.noreply.github.com>
2023-10-31 10:48:38 -0600

    Completed setup of basic game engine

--- main.ts
+++ main.ts
@@ -17,21 +17,17 @@ let tutroialBtnLabel:any
 
 // sprite vars
 let player: any
-let basicEnemy: any
-
+let basicenemys: Sprite[] = []
 
 /* -- "Execution" -- */
 
-/* Functions */
 
 
-function destroyAll() {
-    sprites.destroyAllSpritesOfKind(SpriteKind.Button)
-    sprites.destroyAllSpritesOfKind(SpriteKind.Text)
-    sprites.destroyAllSpritesOfKind(SpriteKind.Player)
-    sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
-}
+/* -Functions- */
 
+
+
+/* Menue Functions */
 
 // function for the main menue
 function mainMenue() {
@@ -78,82 +74,108 @@ function tutorial() {
 
 }
 
+
+
+/* Engine Functions */
+
+
+// function for destroying all sprites on the screen
+function destroyAll() {
+    sprites.destroyAllSpritesOfKind(SpriteKind.Button)
+    sprites.destroyAllSpritesOfKind(SpriteKind.Text)
+    sprites.destroyAllSpritesOfKind(SpriteKind.Player)
+    sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
+}
+
 // function for basic platformer setup
 function platformerSetup() {
     
     // player set up
     player = sprites.create(assets.image`Player`, SpriteKind.Player)
-    controller.moveSprite(player, 100, 0)
-    scene.cameraFollowSprite(player)
+    controller.moveSprite(player, 100, 0) // move left/rght
+    scene.cameraFollowSprite(player)      // camra follow player
+    player.ay = 500                       // make player fall
 
     // jump set up
-    controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-        if (player.isHittingTile(CollisionDirection.Bottom)) {
-            player.vy = -200
+    controller.up.onEvent(ControllerButtonEvent.Pressed, function () { // up button pressed
+        if (player.isHittingTile(CollisionDirection.Bottom)) {         // if the sprite is on the floor
+            player.vy = -200 // jump
         }
     })
-    controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-        if (player.isHittingTile(CollisionDirection.Bottom)) {
-            player.vy = -200
-        }
-    })
-}
-
-
-
-
-function lvl1() {
-
-    player.ay = 500
-
-    platformerSetup()
-
-    // scene stup
-    scene.setTileMapLevel(assets.tilemap`level1`)
-
-    let enemys: any = []
-    let enemyNumber:number
-    for (let i: number = 0; i < 3; i++) {
-        // enemy setup
-        enemyNumber = i
-        enemys[enemyNumber] = sprites.create(assets.image`Basic Enemy`, SpriteKind.Enemy)
-        enemys[enemyNumber].vx = -30
-    
-        // enemy movment setup
-        game.onUpdateInterval(200, function () { // evry 0.2 sec          
-            if (enemys[enemyNumber].vx == 30) {  // if the player is traveling right
-                if (!(tiles.tileAtLocationIsWall(enemys[enemyNumber].tilemapLocation().getNeighboringLocation(CollisionDirection.Right).getNeighboringLocation(CollisionDirection.Bottom)))) { // if the tile to the below and to the right of the sprite is not a wall
-                    enemys[enemyNumber].vx = -30 // make the sprite turn the other direction
-                }
-            } else {
-                if (!(tiles.tileAtLocationIsWall(enemys[enemyNumber].tilemapLocation().getNeighboringLocation(CollisionDirection.Left).getNeighboringLocation(CollisionDirection.Bottom)))) { // if the tile to the below and to the right of the sprite is not a wall
-                    enemys[enemyNumber].vx = 30 // make the sprite turn the other direction
-                }
-            }
-        })
-    }
-    
-    tiles.placeOnTile(enemys[1], tiles.getTileLocation(10, 14))
-    tiles.placeOnTile(enemys[2], tiles.getTileLocation(20, 14))
-    tiles.placeOnTile(enemys[3], tiles.getTileLocation(29, 11))
 
     // colisions
     sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite: Sprite, otherSprite: Sprite) {       // when the playeer overlaps with the basic enemy
         if (sprite.vy >0 && (!(sprite.isHittingTile(CollisionDirection.Bottom)) || sprite.y > otherSprite.top)) { // if the player landed on the enemy
+            player.vy = -100
             otherSprite.destroy() // destroy the enemy
         } else {
             sprite.destroy() // destroy the player
         }
     })
+}
+
+// function for basic enemy setup
+function BscEnSetup(numOEn: number) {
+    //enemy setup
+    for (let i = 0; i <= numOEn; i++) {
+        basicenemys[i] = sprites.create(assets.image`Basic Enemy`, SpriteKind.Enemy)
+        basicenemys[i].vx = -30
+    }
+
+    // enemy movment setup
+    game.onUpdateInterval(200, function () { // evry 0.2 sec          
+        for (let i = 0; i <= numOEn; i++) {
+            if (basicenemys[i].vx == 30) {  // if the player is traveling right
+                if (!(tiles.tileAtLocationIsWall(basicenemys[i].tilemapLocation().getNeighboringLocation(CollisionDirection.Right).getNeighboringLocation(CollisionDirection.Bottom)))) { // if the tile to the below and to the right of the sprite is not a wall
+                    basicenemys[i].vx = -30 // make the sprite turn the other direction
+                }
+            } else {
+                if (!(tiles.tileAtLocationIsWall(basicenemys[i].tilemapLocation().getNeighboringLocation(CollisionDirection.Left).getNeighboringLocation(CollisionDirection.Bottom)))) { // if the tile to the below and to the right of the sprite is not a wall
+                    basicenemys[i].vx = 30 // make the sprite turn the other direction
+                }
+            }
+        }
+    })
+}
+
+function reLocate(srit: any, x: number, y: number) {
+    tiles.placeOnTile(srit, tiles.getTileLocation(x, y))
+}
+
+
+
+
+/* Level Functions*/
+
+
+function lvl1() {
+
+    // scene stup
+    scene.setTileMapLevel(assets.tilemap`level1`) // load tilemap
+
+    // game setup
+    platformerSetup() // setup player
+
+    reLocate(player, 1, 14) // relocate the player
+
+    // enemy setup
+    BscEnSetup(3) // spawn basic enemys
+
+    // relocate enemys
+    reLocate(basicenemys[1], 10, 14)
+    reLocate(basicenemys[2], 20, 14)
+    reLocate(basicenemys[3], 29, 11)
         
 }
 
 
-/*Begin Game*/
+/* -Begin Game- */
 
-scene.setBackgroundImage(assets.image`Intro Image`)
+// intro image
+scene.setBackgroundImage(assets.image`Intro Image`) // load the intro image
+pause(1000);                                        // wait a few second so the player can see the image
+scene.setBackgroundImage(null)                      // get rid of the image
 
-pause(1000);
-scene.setBackgroundImage(null)
+// stat game
+mainMenue() // load the menue
 
-mainMenue() 
